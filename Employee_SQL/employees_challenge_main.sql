e79ae1122d1607936d0a81fe6786c4ac045e7fb6
RAFKAMI <reyhaneh.afkhami@outlook.com>
2023-05-04 15:48:00 -0500

    update

--- Employee_SQL/employees_challenge_main.sql
+++ Employee_SQL/employees_challenge_main.sql
@@ -17,7 +17,7 @@ SELECT * FROM titles
 
 CREATE TABLE employees(
 	emp_no SERIAL PRIMARY KEY,
-	emp_title_id VARCHAR,
+	emp_title_id VARCHAR(30) NOT NULL,
 	birth_date DATE,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
